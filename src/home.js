b2be43999025d3ca757bb4785c167f6ee3d0e244
Dev-J0 <dityx.obsidian@gmail.com>
2025-11-21 14:45:54 +0100

    Updates

---
+++ src/home.js
@@ -0,0 +1,28 @@
+
+import { hideAllSections  } from "./hideSections";
+
+
+
+export function home() {
+    hideAllSections();
+
+    let homeContainer = document.getElementById("homeContainer");
+    if (!homeContainer) {
+        homeContainer = document.createElement("div");
+        homeContainer.id = "homeContainer";
+        homeContainer.className = "homeContainer fade-in";
+
+        const homeTitle = document.createElement("h1");
+        homeTitle.innerText = "Welcome to Our Restaurant";
+
+        const homeText = document.createElement("p");
+        homeText.innerText = "Enjoy our delicious food and cozy atmosphere.";
+
+        homeContainer.appendChild(homeTitle);
+        homeContainer.appendChild(homeText);
+
+        document.getElementById("content").appendChild(homeContainer);
+    }
+
+    homeContainer.hidden = false;
+}
